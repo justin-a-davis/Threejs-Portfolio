32f19d211bf01b99f0228bc6fb7d5593e44a7a74
justin-a-davis <justin.a.davis42@gmail.com>
2022-06-30 13:41:15 -0400

    Spinning David bust

---
+++ main.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+// Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+const loader = new GLTFLoader();
+
+let david = new THREE.Group;
+
+loader.load(
+  'david/david.gltf',
+  function (gltf) {
+    david = gltf.scene;
+    scene.add(gltf.scene);
+  }
+);
+
+const pointLight = new THREE.PointLight(0xffffff);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  david.rotateY(0.01);
+
+  renderer.render(scene, camera);
+
+}
+
+animate();
